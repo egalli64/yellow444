cde4e2d4cd15ca4dd90ab1868fefd94b6385d5b5
Erika Alteri <alterierika@gmail.com>
2021-12-03 08:27:03 +0100

    servlet funzionalità aggiornare/chiedere vuote

---
+++ src/main/java/srv/funzionalità_aggiornare.java
@@ -0,0 +1,27 @@
+package srv;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/yellow/azienda/funzionalità_aggiornare")
+public class funzionalità_aggiornare extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String nome = request.getParameter("nome");
+		request.setAttribute("nome", nome);
+		String quantità = request.getParameter("quantità");
+		request.setAttribute("quantità", quantità);
+		
+		request.getRequestDispatcher("").forward(request, response);
+	
+	}
+
+}
---
+++ src/main/java/srv/funzionalità_chiedere.java
@@ -0,0 +1,24 @@
+package srv;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/funzionalità_chiedere")
+public class funzionalità_chiedere extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+	
+		String materiale = request.getParameter("materiale");
+		request.setAttribute("materiale", materiale);
+		
+		
+		request.getRequestDispatcher("").forward(request, response);
+	}
+
+}
