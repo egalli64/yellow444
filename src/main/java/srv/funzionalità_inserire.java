78e7c7cea594a56a06492ce121eef123fbb80148
Erika Alteri <alterierika@gmail.com>
2021-12-03 08:19:46 +0100

    servlet funzionalità inserire vuota

---
+++ src/main/java/srv/funzionalità_inserire.java
@@ -0,0 +1,27 @@
+package srv;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("yellow/azienda/funzionalità_inserire")
+public class funzionalità_inserire extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String nome = request.getParameter("nome");
+		request.setAttribute("nome", nome);
+		String materiale = request.getParameter("materiale");
+		request.setAttribute("materiale", materiale);
+		String quantità = request.getParameter("quantità");
+		request.setAttribute("quantità", quantità);
+		
+		request.getRequestDispatcher("").forward(request, response);
+	}
+	
+}
